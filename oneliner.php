049308f0e7ac42afc479718ed113eb007f22d7f7
tinou <nobody@example.com>
2010-10-13 22:12:43 +0200

    IEG style

--- oneliner.php
+++ oneliner.php
@@ -27,23 +27,64 @@ function handle_post_data() {
   }
 }
 
+$css = <<<CSS
+input {
+       font-family: Verdana, Arial, Helvetica, sans-serif;
+       font-size: 11px;
+       color: #000000;
+       border-width: 1px;
+       border-style: solid;
+       border-color: #000000;
+       background-color: #fffebe;
+      }
+
+BODY { 
+  background-color: #fffebe;
+  scrollbar-face-color: #fffebe;
+  scrollbar-shadow-color: #CCCCCC;
+  scrollbar-3dlight-color: #fffebe;
+  scrollbar-track-color: #fffebe;
+  scrollbar-arrow-color:#CCCCCC;
+  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px; color: #000000; text-decoration: none 
+      }
+
+.pseudo {
+  color: #FF6600
+}
+
+.pseudobracket {
+  color: #7DBFF1
+}
+
+CSS;
+
 handle_post_data();
 
 $d = read_data();
 $rev_d = array_reverse ($d);
 
-echo '<form method=post>';
-echo '<input type=text name=pseudo value=Pseudo></input>';
-echo '<input type=text name=message></input>';
-echo '<input type=submit name=submit value=Go></input>';
+echo '<html><head><title>Oneliner</title>';
+
+echo '<style type="text/css">';
+echo $css;
+echo '</style></head><body>';
+
+echo '<form method="post">';
+echo '<input type="text" name="pseudo" value="Pseudo"></input>';
+echo '<input type="text" name="message"></input>';
+echo '<input type="submit" name="submit" value="Go"></input>';
 echo '</form>';
 
 echo '<ul>';
 foreach ($rev_d as $line) {
   echo '<li>';
-  echo $line[0].'['.$line[1].']'.$line[2];
+  echo '<span class="pseudobracket">[</span>'
+    .'<span class="pseudo">' . $line[1] . '</span>'
+    .'<span class="pseudobracket">]</span>'
+    .'<span class="message">' . $line[2] . '</span>';
   echo '</li>';
 }
 echo '</ul>';
+echo '</body></html>';
 
 ?>
