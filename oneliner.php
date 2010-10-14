3e755be2d873218b7f4a92795d591312541f996c
tinou <nobody@example.com>
2010-10-14 13:07:09 +0200

    pagination

--- oneliner.php
+++ oneliner.php
@@ -2,6 +2,10 @@
 
 $php5 = 1;
 
+$limit = 100;
+
+$url = $_SERVER['REQUEST_URI'];
+
 if (!function_exists('fputcsv')) {
 function fputcsv($fh, $arr)
 {
@@ -42,7 +46,14 @@ function handle_post_data() {
     $message = $_POST['message'];
     $time    = time();
     write_data(array($time, $pseudo, $message));
-    header('Location: ' . ($_SERVER['REQUEST_URI']));
+    header('Location: ' . $url);
+  }
+}
+
+function handle_get_data() {
+  if (array_key_exists('chuck_norris', $_GET)) {
+    global $limit;
+    $limit = -1;
   }
 }
 
@@ -78,6 +89,7 @@ BODY {
 CSS;
 
 handle_post_data();
+handle_get_data();
 
 $d = read_data();
 $rev_d = array_reverse ($d);
@@ -88,16 +100,22 @@ echo '<style type="text/css">';
 echo $css;
 echo '</style></head><body>';
 
-echo '<img src="oneliner.jpg" height="40" />';
+echo '<a href="'.$url.'?chuck_norris"><img style="border:none" src="oneliner.jpg" height="40" /></a>';
 
 echo '<form method="post">';
 echo '<input type="text" size="8" maxlength="15" name="pseudo" value="pseudo"></input>';
 echo '<input type="text" size="25" name="message" value="message-pourri" onclick="this.value=\'\'"></input>';
 echo '<input type="submit" name="submit" value="+"></input>';
 echo '</form>';
-
 echo '<ul>';
+$displayed = 0;
 foreach ($rev_d as $line) {
+  if ($limit >= 0) {
+    $displayed++;
+    if ($displayed > $limit) {
+      break;
+    }
+  }
   if ($php5 == 0) {
     $line[1] = stripslashes($line[1]);
     $line[2] = stripslashes($line[2]);
